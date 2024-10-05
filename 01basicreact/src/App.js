f4667ae3325565c0a3a93f8f02d7ec26ef85e391
prathmeshborate <prathmeshborate01@gmail.com>
2024-10-05 12:25:17 +0530

    structure learning

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,7 +1,10 @@
-
+import Chai from './Chai'
 function App() {
   return (
+    <>
+    <Chai/>
     <h1>Chai aur react</h1>
+    </>
   );
 }
 
--- 01bvitereact/src/App.jsx
+++ 01bvitereact/src/App.jsx
@@ -1,8 +1,12 @@
-
+import Chai from "./chai"
 function App() {
   
   return (
-    <h1>Chai aur ract with vite | Prath</h1>
+    <>
+    <Chai/>
+    <h1> chai aur react</h1>
+    <p>test para</p>
+    </>
   )
 }
 
