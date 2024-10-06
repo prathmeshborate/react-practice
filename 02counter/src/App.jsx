a53ca59fcf9d7ee6b364334738f52effd46d8e34
prathmeshborate <prathmeshborate01@gmail.com>
2024-10-06 12:43:39 +0530

    batch, fiber, diffing algorithm

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -5,13 +5,29 @@ import './App.css'
 
 function App() {
 
-  let [counter, setCounter] = useState(15)
+  const [counter, setCounter] = useState(15)
 
   //counter is variable and setCounter is function
 
-  const addValue = () => setCounter(counter + 1)
+  const addValue = () => {
+    
+    /* setCounter(counter + 1) this is for 
+    adding for only once even if you duplicate
+    this it'll add 1 to counter inly once coz 
+    hoook sends work in batches and duplicate 
+    batches are neglected. fiber gains more 
+    control on batches, and diffing algorithm
+     also */
 
-  const substractValue = () => setCounter(counter - 1)
+    setCounter(counter => counter + 1) // we are passing arrow function here
+    setCounter(counter => counter + 1)
+    setCounter(counter => counter + 1)
+    setCounter(counter => counter + 1)
+  }
+
+  const substractValue = () => {
+    setCounter(counter - 1)
+  }
 
   return (
   <>
