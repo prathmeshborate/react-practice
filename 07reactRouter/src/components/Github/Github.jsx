f4357829befdcdf984736f313137448a4c454145
Prathmesh Dilip Borate <prathmeshborate01@gmail.com>
2024-10-11 16:39:46 +0000

    final done

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
-
+import { useLoaderData } from "react-router-dom";
 function Github() {
-    const [data, setData] = useState([])
-    useEffect(() => {
-        fetch('https://api.github.com/users/prathmeshborate')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            setData(data)
-        })
-    }, [])
+    const data = useLoaderData ()
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/prathmeshborate')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data);
+    //         setData(data)
+    //     })
+    // }, [])
 
     return (
         <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">Github followers: {data.followers}
@@ -19,3 +20,8 @@ function Github() {
 }
 
 export default Github
+
+export const githubInfoLoader = async() => {
+    const response = await fetch('https://api.github.com/users/prathmeshborate')
+    return response.json()
+}
